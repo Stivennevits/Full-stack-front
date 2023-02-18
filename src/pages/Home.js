0732d600b946ff2c1cf3dc4a68fa60ed7d0e0ff0
stivennevits <stivennevits16@gmail.com>
2023-02-17 22:44:52 -0500

    se conecta con springboot por axios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Home() {
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        loadUsers();
+    })
+
+    const loadUsers = async () => {
+        const result = await axios.get("http://localhost:8080/users")
+        console.log(result.data)
+    }
+
   return (
     <div className='container'>
         <div className='py-4'>
