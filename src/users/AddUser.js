6855425f299a2bdbc90da0f52039fc61b9aca97f
stivennevits <stivennevits16@gmail.com>
2023-02-18 10:31:31 -0500

    se crear formulario adduser

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -2,8 +2,54 @@ import React from 'react'
 
 export default function AddUser() {
   return (
-    <div>
-        AddUser Form
+    <div className='container'>
+        <div className='row'>
+            <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
+                <h2 className='text-center m-4 '> Register User </h2>
+                <div className='mb-3'>
+                    <label htmlFor='Name' className='form-label '>
+                        Name
+                    </label>
+                    <input
+                        type={"text"}
+                        className="form-control shadow"
+                        placeholder='Enter your Name'
+                        name='name'
+                        />
+                </div>
+
+                <div className='mb-3'>
+                    <label htmlFor='Username' className='form-label'>
+                    Username
+                    </label>
+                    <input
+                        type={"text"}
+                        className="form-control shadow"
+                        placeholder='Enter your Username'
+                        name='username'
+                        />
+                </div>
+
+                <div className='mb-3'>
+                    <label htmlFor='Email' className='form-label'>
+                         Email
+                    </label>
+                    <input
+                        type={"text"}
+                        className="form-control shadow"
+                        placeholder='Enter your e-mail address'
+                        name='email'
+                        />
+                </div>
+                <button type='submit' className='btn btn-outline-primary shadow'>
+                Submit
+                </button>
+                <button type='submit' className='btn btn-outline-danger mx-2 shadow' >
+                Cancel
+                </button>
+            </div>
+            
+        </div>
     </div>
   )
 }
