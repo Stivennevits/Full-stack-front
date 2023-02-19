892ab8ae16cd33e37d19791ad8b9c55e7ad7d515
stivennevits <stivennevits16@gmail.com>
2023-02-18 20:57:42 -0500

    se da funcionabilidad a boton delete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 export default function Home() {
     const [users, setUsers] = useState([])
+
+    const {id} = useParams()
 
     useEffect(() => {
         loadUsers();
     })
 
-    const loadUsers = async () => {
+    const loadUsers = async (id) => {
         const result = await axios.get("http://localhost:8080/users")
         setUsers(result.data)
+    };
+
+    const deleteUSer = async (id) => {
+        await axios.delete(`http://localhost:8080/user/${id}`)
+        loadUsers()
     }
+
+
 
   return (
     <div className='container'>
@@ -44,7 +53,12 @@ export default function Home() {
                       >
                       Edit
                     </Link>
-                    <button className='btn btn-danger mx-2'>Delete</button>
+                    <button className='btn btn-danger mx-2'
+                    
+                      onClick={() => deleteUSer(user.id)}
+                        >
+                        Delete
+                    </button>
                 </td>
                 </tr>
             ))
