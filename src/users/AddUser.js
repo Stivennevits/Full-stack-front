17a2e212d2dd8ae1a5e5f9a6806d1cb8f17748a9
stivennevits <stivennevits16@gmail.com>
2023-02-18 19:33:10 -0500

    se crea funcion para btn submit y cancel

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function AddUser() {
+
+    let navigate = useNavigate();
+
+    const [user,setUser]=useState({
+        name:"",
+        username:"",
+        email:""
+    })
+
+    const {name,username,email}= user
+
+    const onInputChange = (e) => {
+        setUser({...user, [e.target.name]:e.target.value})
+    }
+
+    const onSubmit = async (e) =>{
+        e.preventDefault(); 
+        await axios.post("http://localhost:8080/user", user)
+        navigate("/")
+    }
+
   return (
     <div className='container'>
         <div className='row'>
             <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
                 <h2 className='text-center m-4 '> Register User </h2>
+
+                <form onSubmit={(e) => {onSubmit(e)}}>
                 <div className='mb-3'>
                     <label htmlFor='Name' className='form-label '>
                         Name
@@ -15,6 +40,8 @@ export default function AddUser() {
                         className="form-control shadow"
                         placeholder='Enter your Name'
                         name='name'
+                        value={name}
+                        onChange={(e)=>onInputChange(e)}
                         />
                 </div>
 
@@ -27,6 +54,8 @@ export default function AddUser() {
                         className="form-control shadow"
                         placeholder='Enter your Username'
                         name='username'
+                        value={username}
+                        onChange={(e)=>onInputChange(e)}
                         />
                 </div>
 
@@ -39,14 +68,17 @@ export default function AddUser() {
                         className="form-control shadow"
                         placeholder='Enter your e-mail address'
                         name='email'
+                        value={email}
+                        onChange={(e)=>onInputChange(e)}
                         />
                 </div>
                 <button type='submit' className='btn btn-outline-primary shadow'>
                 Submit
                 </button>
-                <button type='submit' className='btn btn-outline-danger mx-2 shadow' >
+                <Link className='btn btn-outline-danger mx-2 shadow' to="/">
                 Cancel
-                </button>
+                </Link>
+                </form>
             </div>
             
         </div>
